cbbb259d0e9192306b5da49f13f100c0691ed045
laumichael73 <32725346+laumichael73@users.noreply.github.com>
2018-09-20 18:23:43 -0400

    First upload

--- Manager.java
+++ Manager.java
@@ -1,14 +1,37 @@
-public class Manager
-{
-  int productCount = 0;
-  Product[] productList;
-  
-  // n is the size of the Product array
-  public Manager(int n)
-  {
-    productList = new Product[n];
-  }
-  
-  
-  
+public class Manager {
+    private int size = 0;
+    private Product[] products;
+
+
+    public Manager() {
+        products = new Product[10];
+    }
+
+    // n is the size of the Product array
+    public Manager(int n) {
+        products = new Product[n];
+    }
+
+    public void addProduct(Product p) {
+        products[size] = p;
+        size = size + 1;
+    }
+
+    public Product getProduct(int i) {
+        return products[i];
+    }
+
+    public Product query(String name) {
+        for (int i = 0; i < size; i++) {
+            if (products[i].getName().equals(name)) {
+                return products[i];
+            }
+        }
+        System.err.println("Error: product not found");
+        return null;
+    }
+
+    public int length() {
+        return size;
+    }
 }
